e3d2565c24133c2c9fc5ed523e4fc9947e072a6b
S Chowdhury <sadikurrc@gmail.com>
2024-12-08 15:27:55 +0000

    Added some pages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import { redirect } from 'next/navigation';
 
 export default function Home() {
   redirect('/dashboard');
-=======
-export default function Home() {
-  return (
-    <main><h1>Hello world</h1></main>
-  );
->>>>>>> 0d3f037 (Populate dashboard and add pages for assessments, marking-suite and submissions)
 }
