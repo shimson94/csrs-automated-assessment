924ef9f8935e66a8a0792d396cee2605303264b9
S Chowdhury <sadikurrc@gmail.com>
2024-12-12 06:42:47 +0000

    test: set up Jest testing environment and add initial tests for MarkingSuite component

--- automarker/jest.config.js
+++ automarker/jest.config.js
@@ -1,12 +1,18 @@
+// jest.config.js
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir: './'
 })
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
 
 module.exports = createJestConfig(customJestConfig)
