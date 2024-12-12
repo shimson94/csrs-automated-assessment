e7b6fdf84a5ddbf3b5faf4ae562b6f430b75ced4
Muzammil Hashim Chaudry <chaudrymh94@gmail.com>
2024-12-12 01:32:27 +0000

    Linked Flask backend and node.js frontend to run concurrently

--- src/api/app.py
+++ src/api/app.py
@@ -5,13 +5,17 @@ from pathlib import Path
 
 from flask import Flask
 from flask_cors import CORS
-from routes import api_blueprint
+from .routes import api_blueprint
 
 app = Flask(__name__)
 CORS(app)  # Enable Cross-Origin Resource Sharing for frontend-backend communication
 
 # Register the API blueprint
 app.register_blueprint(api_blueprint, url_prefix="/api")
+
+@app.route("/", methods=["GET"])
+def root():
+    return "Flask API is running!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
